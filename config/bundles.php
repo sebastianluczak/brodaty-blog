31b917a8d7c7643aee8b13c6a92e04edc165fcfd
Sebastian Łuczak <sebastian.m.luczak@gmail.com>
2022-09-15 18:39:42 +0200

    disqus added

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\DisqusBundle\KnpDisqusBundle::class => ['all' => true],
 ];
